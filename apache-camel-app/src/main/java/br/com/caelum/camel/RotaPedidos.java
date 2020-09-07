76b16f966808a63b54e3ba217e815678e448ef6e
Matheus Silva <msilva.jus@gmail.com>
2020-09-07 10:09:25 -0300

    Error Handler

--- apache-camel-app/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ apache-camel-app/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -2,6 +2,7 @@ package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -20,14 +21,30 @@ public class RotaPedidos {
 			@Override
 			public void configure() throws Exception {
 				
+				errorHandler(deadLetterChannel("file:erro").
+						logExhaustedMessageHistory(true).
+							maximumRedeliveries(3).
+								redeliveryDelay(2000). // wait 2 seconds
+									onRedelivery(new Processor() {
+										@Override
+										public void process(Exchange exchange) throws Exception{
+											int counter = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER);
+											int max = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_MAX_COUNTER);
+											System.out.println("Redelivery " + counter + "/" + max);
+										}
+									})
+								);
+				
 				// PADRÃO FILE SHARING
 				//nomeDoComponente:nomedapasta?parametros
 				from("file:pedidos?delay=5s&noop=true").
+					log("file:name").
 					routeId("rota-pedidos").
-				multicast(). // pega a mensagem do camel e faz um multicast para as subrotas
-					parallelProcessing(). //Configuração que o multicast() possui para processar cada subrota em uma Thread separada
-						to("direct:http"). // direct -> forma que o CAMEL chama uma subrota
-						to("direct:soap");
+					to("validator:pedido.xsd");
+//				multicast(). // pega a mensagem do camel e faz um multicast para as subrotas
+//					parallelProcessing(). //Configuração que o multicast() possui para processar cada subrota em uma Thread separada
+//						to("direct:http"). // direct -> forma que o CAMEL chama uma subrota
+//						to("direct:soap");
 				
 				from("direct:http").
 					routeId("rota-http").
